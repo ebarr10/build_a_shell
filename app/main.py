94823a57bc938fcc083a20209f8a610ea6f34048
Ethan <ej.barr00@gmail.com>
2026-03-06 23:47:02 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
-        sys.stderr.write(f"{command}: command not found")
+        sys.stderr.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
