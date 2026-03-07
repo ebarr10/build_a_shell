436e666389daf0db84d6375b685d5eeedc74558c
Ethan <ej.barr00@gmail.com>
2026-03-06 23:47:54 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
 import sys
 
+commands = {
+    "exit": lambda: sys.exit(0),
+}
+
 
 def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
-        sys.stderr.write(f"{command}: command not found\n")
+        if command in commands:
+            commands[command]()
+        else:
+            sys.stderr.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
