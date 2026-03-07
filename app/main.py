715b677e7536f2999a1dcf3dac83cc51314cfd46
Ethan <ej.barr00@gmail.com>
2026-03-07 00:31:11 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -17,15 +17,14 @@ def command_type_check(command):
         # get PATH and split into paths by delimiter
         path = os.getenv("PATH")
         paths = path.split(os.pathsep)
-        found = False
 
         # Test is executable
         for dir in paths:
             location = os.path.join(dir, command)
             if os.access(location, os.X_OK):
-                print(f"{command} is {location}")
-                found = True
-        if not found:
+                print(f"{command} is {location}\n")
+                break
+        else:
             print(f"{command}: not found\n")
 
 
